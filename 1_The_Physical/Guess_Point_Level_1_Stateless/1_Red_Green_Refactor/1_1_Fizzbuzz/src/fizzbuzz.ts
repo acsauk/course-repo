dc7142360a602ed3fc4b2c9e7fa81be29f43f97d
Alex Saunders <acsauk@gmail.com>
2023-03-26 18:23:39 +0100

    [New] return stringified number when not multiple of 3 or 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,14 +1,14 @@
 const FizzBuzz = {
-    run: (number: number) => {
+    run: (number: number): string => {
         if (number % 5 == 0) {
             return 'Buzz'
         }
-        
+
         if (number % 3 == 0) {
             return 'Fizz'
         }
 
-        return 'test'
+        return number.toString()
     }
 }
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -2,8 +2,11 @@ import {describe, it, expect} from '@jest/globals'
 import FizzBuzz from './fizzbuzz'
 
 describe('fizzbuzz', () => {
-    it('returns a string', () => {
-        expect(typeof FizzBuzz.run(1) == 'string').toEqual(true)
+    it('returns the number passed in', () => {
+        const tcs: number[] = [1,2,4]
+        tcs.forEach((n) => {
+            expect(FizzBuzz.run(n)).toEqual(n.toString())    
+        });
     })
 
     it('multiple of 3 return Fizz', () => {
