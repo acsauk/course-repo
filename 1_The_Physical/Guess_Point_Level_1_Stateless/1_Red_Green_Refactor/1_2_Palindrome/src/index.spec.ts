b4ff33456e53f0eff48efaf16fe3b7c304d6a217
Alex Saunders <acsauk@gmail.com>
2023-03-28 16:00:54 +0100

    [Refactor] drop redundanttest

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,10 +1,6 @@
 import {check} from './'
 
 describe('palindrome checker', () => {
-    it('returns a boolean', () => {
-        expect(check('wow')).toEqual(true)
-    })
-
     it('knows which single words are palindromes', () => {
         let tcs: string[] = ['wow', 'mom', 'tattarrattat']
 
