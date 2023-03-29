cf45f953e5571b6fa1682912dc06930dd386ad8b
Alex Saunders <acsauk@gmail.com>
2023-03-29 12:00:54 +0100

    [Refactor] rework for clarity

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -6,11 +6,6 @@ export type ValidationResult = {
 
 export class PasswordValidator {    
     validate(password: string): ValidationResult {
-        const validated: ValidationResult = {
-            result: true,
-            error: ''
-        }
-
         const errors: string[] = []
 
         if (password.length < 5) {
@@ -21,25 +16,41 @@ export class PasswordValidator {
             errors.push('too long')
         }
 
-        if (!/\d/.test(password)) {
+        if (!hasDigit(password)) {
             errors.push('needs a digit')
         }
 
-        if (!/[A-Z]/.test(password)) {
+        if (!hasUppercaseCharacter(password)) {
             errors.push('needs an uppercase character')
         }
 
-        let error: string = ''
-
-        if (errors.length > 0) {
-            error = 'Your password is not valid:\n'
-            errors.forEach((err, index) => {
-                error += index < (errors.length - 1) ? `- ${err}\n` : `- ${err}`
-            })
-            validated.result = false
-        }
-
-        validated.error = error
-        return validated
+        return buildValidationResult(errors)
     }
+}
+
+function hasDigit(password: string): boolean {
+    return /\d/.test(password)
+}
+
+function hasUppercaseCharacter(password: string): boolean {
+    return /[A-Z]/.test(password)
+}
+
+function buildValidationResult(errors: string[]): ValidationResult {
+    const validated: ValidationResult = {
+        result: true,
+        error: ''
+    }
+
+    if (errors.length > 0) {
+        let error = 'Your password is not valid:\n'
+        errors.forEach((err, index) => {
+            error += index < (errors.length - 1) ? `- ${err}\n` : `- ${err}`
+        })
+        
+        validated.error = error
+        validated.result = false
+    }
+
+    return validated
 }
