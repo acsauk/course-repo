a32de293b1da08c6cc71d7b5b6bd2d0c2277aa52
Alex Saunders <acsauk@gmail.com>
2023-05-06 15:07:45 +0100

    [New:Red] add test for minimal input to scope API

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,12 @@
+import { calculateStats, Result } from "./index"
 
 describe('stats calculator', () => {
+    it('returns a Result object with minimumVal, maximumVal, elmentCount and average props', () => {
+        let result: Result = calculateStats([0])
 
-
+        expect(result.minimumVal).toEqual(0)
+        expect(result.maximumVal).toEqual(0)
+        expect(result.elmentCount).toEqual(1)
+        expect(result.average).toEqual(0.0)
+    })
 })
