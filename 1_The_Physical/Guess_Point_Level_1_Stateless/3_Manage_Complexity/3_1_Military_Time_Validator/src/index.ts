578946fface85f859a58f1821327af534df046f6
Alex Saunders <acsauk@gmail.com>
2023-06-16 14:28:12 +0100

    [Refactor] parseFloat once for each time

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,15 +1,17 @@
 export function ValidateTimeRange(timeRange: string) {
     const times = timeRange.replaceAll(':', '.').split(' - ')
+    const firstTime = parseFloat(times[0])
+    const secondTime = parseFloat(times[1])
 
-    if (parseFloat(times[0]) > 23.59 || parseFloat(times[0]) < 0) {
+    if (firstTime > 23.59 || firstTime < 0) {
         return false
     }
 
-    if (parseFloat(times[1]) > 23.59 || parseFloat(times[1]) < 0) {
+    if (secondTime > 23.59 || secondTime < 0) {
         return false
     }
 
-    if (parseFloat(times[1]) < parseFloat(times[0])) {
+    if (secondTime < firstTime) {
         return false
     }
 
