4ec962c23d29aac94bf2078f1fe672d82f6b3ebf
Alex Saunders <acsauk@gmail.com>
2023-06-16 12:42:38 +0100

    [New:Green] Implement simple check on first time being a valid military time

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,9 @@
 export function ValidateTimeRange(timeRange: string) {
+    const times = timeRange.replaceAll(':', '.').split(' - ')
+
+    if (parseFloat(times[0]) > 23.59) {
+        return false
+    }
+
     return true
 }
