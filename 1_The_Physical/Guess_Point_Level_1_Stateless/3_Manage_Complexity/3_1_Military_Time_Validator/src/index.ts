b80fc49109f220d7e8ddeaa1db9344259381076e
Alex Saunders <acsauk@gmail.com>
2023-06-16 14:37:34 +0100

    [Refactor] Replace magic floats with consts for context

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,12 +2,14 @@ export function ValidateTimeRange(timeRange: string) {
     const times = timeRange.replaceAll(':', '.').split(' - ')
     const firstTime = parseFloat(times[0])
     const secondTime = parseFloat(times[1])
+    const lowerBoundaryTime = 0
+    const upperBoundaryTime = 23.59
 
-    if (firstTime > 23.59 || firstTime < 0) {
+    if (firstTime > upperBoundaryTime || firstTime < lowerBoundaryTime) {
         return false
     }
 
-    if (secondTime > 23.59 || secondTime < 0) {
+    if (secondTime > upperBoundaryTime || secondTime < lowerBoundaryTime) {
         return false
     }
 
