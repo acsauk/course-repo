98b92bc360d3f137b4006f115a5f33d11ee05d0f
Alex Saunders <acsauk@gmail.com>
2023-06-16 12:54:22 +0100

    [New:Red] Test for first time being a valid military time

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -8,4 +8,12 @@ describe('military time validator', () => {
     it('should return false for 25:00 - 12:23', () => {
         expect(ValidateTimeRange('25:00 - 12:23')).toBeFalsy()
     })
+
+    it.each([
+        '25:00 - 12:23',
+        '-10:00 - 12:23',
+        '100:00 - 12:23',
+    ])('should return false if the first time in the range is not a valid military time', (timeRange) => {
+        expect(ValidateTimeRange(timeRange)).toBeFalsy()
+    })
 })
