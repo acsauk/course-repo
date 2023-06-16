0b41572ae95835dc3a60cf2f92c301af6eeb4a36
Alex Saunders <acsauk@gmail.com>
2023-06-16 12:24:59 +0100

    [New:Red] Add initial test for a valid time range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { ValidateTimeRange } from "./index"
 
 describe('military time validator', () => {
-
+    it('should return true for 00:00 - 00:01', () => {
+        expect(ValidateTimeRange('00:00 - 00:01')).toBeTruthy()
+    })
 
 })
