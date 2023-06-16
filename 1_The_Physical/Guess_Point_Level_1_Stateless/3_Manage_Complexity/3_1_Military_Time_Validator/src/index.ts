6d6151871ac347b5140b23c2bf39760f4be59981
Alex Saunders <acsauk@gmail.com>
2023-06-16 14:02:34 +0100

    [New:Green] Validate second time range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -5,5 +5,9 @@ export function ValidateTimeRange(timeRange: string) {
         return false
     }
 
+    if (parseFloat(times[1]) > 23.59 || parseFloat(times[1]) < 0) {
+        return false
+    }
+
     return true
 }
