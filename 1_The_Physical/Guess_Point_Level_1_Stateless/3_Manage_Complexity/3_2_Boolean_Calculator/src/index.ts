8c043aec6593fc3f1dcd2e4adcf77699a3ea7ce1
Alex Saunders <acsauk@gmail.com>
2023-06-16 18:24:13 +0100

    [New:Green] support negating true and false with not

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,5 +1,24 @@
 export class Calculator {
     static calculate(expression: string): boolean {
-        return expression == 'TRUE'
+        const expressions = expression.split(' ')
+        
+        let finalExpression = false
+        let negate = false
+
+        expressions.forEach((item: string) => {
+            if (item === 'NOT') {
+                negate = true
+            }
+
+            if (item === 'TRUE') {
+                finalExpression = negate ? false : true
+            }
+
+            if (item === 'FALSE') {
+                finalExpression = negate ? true : false
+            }
+        })
+
+        return finalExpression
     }
 }
