3e970342ff4d8f1e06589e5c14bbdd7dbe0b6752
Alex Saunders <acsauk@gmail.com>
2023-06-16 13:58:39 +0100

    [New:Green] Validate on time range being in valid military format

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,7 +1,7 @@
 export function ValidateTimeRange(timeRange: string) {
     const times = timeRange.replaceAll(':', '.').split(' - ')
 
-    if (parseFloat(times[0]) > 23.59) {
+    if (parseFloat(times[0]) > 23.59 || parseFloat(times[0]) < 0) {
         return false
     }
 
