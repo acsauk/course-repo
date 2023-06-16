7fb688b1e1fae4153561dbe2c86837a18c7d94b2
Alex Saunders <acsauk@gmail.com>
2023-06-16 12:27:50 +0100

    [New:Red] Add testcase for invalid range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -4,4 +4,8 @@ describe('military time validator', () => {
     it('should return true for 01:12 - 14:32', () => {
         expect(ValidateTimeRange('01:12 - 14:32')).toBeTruthy()
     })
+
+    it('should return false for 25:00 - 12:23', () => {
+        expect(ValidateTimeRange('25:00 - 12:23')).toBeFalsy()
+    })
 })
